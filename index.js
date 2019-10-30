1e6785fd20402f987a806e6756645fc1a93f3d9b
Damien Hampton <damien.hampton@gmail.com>
2019-10-30 21:26:01 +0000

    end chapter 19

--- index.js
+++ index.js
@@ -5,17 +5,27 @@ function TestCase(name){
   this.name = name;
 }
 
+TestCase.prototype.setUp = function(){
+  //nothing
+}
+
 TestCase.prototype.run = function(){
+  this.setUp();
   const method = this[this.name].bind(this);
   method();
 }
 
 function WasRun(name){
-  this.wasRun = false;
+  this.wasSetUp = false;
   TestCase.call(this, name);
 }
 
 WasRun.prototype = Object.create(TestCase.prototype);
+
+WasRun.prototype.setUp = function(){
+  this.wasRun = false;
+  this.wasSetUp = true;
+}
 
 WasRun.prototype.testMethod = function(){
   this.wasRun = true;
@@ -27,11 +37,19 @@ function TestCaseTest(name){
 
 TestCaseTest.prototype = Object.create(TestCase.prototype);
 
+TestCaseTest.prototype.setUp = function(){
+  this.test = new WasRun('testMethod');
+}
+
 TestCaseTest.prototype.testRunning = function(){
-  const test = new WasRun('testMethod');
-  assert.isFalse(test.wasRun);
-  test.run();
-  assert.isTrue(test.wasRun);
+  this.test.run();
+  assert.isTrue(this.test.wasRun);
+}
+
+TestCaseTest.prototype.testSetUp = function(){
+  this.test.run();
+  assert.isTrue(this.test.wasSetUp);
 }
 
 new TestCaseTest('testRunning').run();
+new TestCaseTest('testSetUp').run();
